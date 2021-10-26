d4f7a922dd128227fd1676aa4a9d16150e3946b3
AlfredWangTJ <92712829+AlfredWangTJ@users.noreply.github.com>
2021-10-26 15:55:16 +0800

    Add files via upload

---
+++ webcomponent.js
@@ -0,0 +1,30 @@
+(function()  {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+    `;
+
+    customElements.define('test.broadcaster', class WidgetTemplate extends HTMLElement {
+
+
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._msg="This is a default msg.";
+            this.dom;
+            this.addEventListener("dblclick",event => {
+				var event = new CustomEvent("sendMsg",{
+					detail: {
+						properties: {
+							msg: this.msg
+						}
+					}
+			});
+				this.dispatchEvent(event);
+			});
+		}
+        set msg(val){this._msg=val;}
+        get msg(){return this._msg;}
+        });
+    })();
+        
