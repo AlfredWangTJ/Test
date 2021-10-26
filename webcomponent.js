9b8066a4efe11f88a02c893558ecde23a8ce2eec
AlfredWangTJ <92712829+AlfredWangTJ@users.noreply.github.com>
2021-10-26 16:25:45 +0800

    Add files via upload

--- webcomponent.js
+++ webcomponent.js
@@ -1,32 +1,35 @@
-(function()  {
-    let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
+(function () {
+  let tmpl = document.createElement("template");
+  tmpl.innerHTML = `
+  <div width="100px" height="100px" border="1px" color="black"></div>
     `;
 
-    customElements.define('test-broadcaster', class WidgetTemplate extends HTMLElement {
-
-
-		constructor() {
-			super(); 
-			let shadowRoot = this.attachShadow({mode: "open"});
-			shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._msg="This is a default msg.";
-            this.dom;
-            this.addEventListener("dblclick",event => {
-
-				var event = new CustomEvent("sendMsg");
-            //     ,{
-			// 		detail: {
-			// 			properties: {
-			// 				msg: this.msg
-			// 			}
-			// 		}
-			// });
-				this.dispatchEvent(event);
-			});
-		}
-        set msg(val){this._msg=val;}
-        get msg(){return this._msg;}
+  customElements.define(
+    "test-broadcaster",
+    class WidgetTemplate extends HTMLElement {
+      constructor() {
+        super();
+        let shadowRoot = this.attachShadow({ mode: "open" });
+        shadowRoot.appendChild(tmpl.content.cloneNode(true));
+        this._msg = "This is a default msg.";
+        this.dom;
+        this.addEventListener("dblclick", (event) => {
+          var event = new CustomEvent("sendMsg", {
+            detail: {
+              properties: {
+                msg: this.msg,
+              },
+            },
+          });
+          this.dispatchEvent(event);
         });
-    })();
-        
+      }
+      set msg(val) {
+        this._msg = val;
+      }
+      get msg() {
+        return this._msg;
+      }
+    }
+  );
+})();
